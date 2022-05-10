07bf83e4eed352d1dcb1e3d9892e33bc79424ec6
Meli <melanie.wolf@hak-bregenz.at>
2022-05-10 21:56:53 +0200

    tierarten filter funktioniert

--- TierheimDhiliUndJustus/DAL/Tier_DA.cs
+++ TierheimDhiliUndJustus/DAL/Tier_DA.cs
@@ -84,5 +84,57 @@
             return neuesTier;
 
         }
+
+        public static List<Tier> GetTierWithFilter(List<int> checkboxes)
+        {
+            string whereStatement = "";
+            List<Tier> tierList = new List<Tier>();
+            if (checkboxes.Count != 0)
+            {
+                whereStatement = "WHERE tierrasse.FK_Tierart_Tierrasse IN(";
+                foreach (int item in checkboxes)
+                {
+                    whereStatement += item;
+                    if (item != checkboxes.Last())
+                    {
+                        whereStatement += ", ";
+                    }
+                }
+                whereStatement += ")";
+            }
+
+            using (MySqlConnection conn = new MySqlConnection(Config.CONNSTRING))
+            {
+                conn.Open();
+                sqlstatement = "SELECT * FROM tier JOIN tierrasse ON tier.FK_Tierrasse_Tier = tierrasse.ID_Tierrasse " + whereStatement;
+
+
+
+                using (MySqlCommand cmd = new MySqlCommand(sqlstatement, conn))
+                {
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Tier tier = new Tier(
+                            (int)reader["ID_Tier"],
+                            (string)reader["Tiername"],
+                            (DateTime)reader["Geburtsdatum"],
+                            (string)reader["Geschlecht"],
+                            (string)reader["Beschreibung"],
+                            (SByte)reader["Fundtier"],
+                            (int)reader["FK_Tierrasse_Tier"]); ;
+
+                            tierList.Add(tier);
+                        }
+                    }
+                }
+            }
+
+            whereStatement = "";
+            return tierList;
+
+        }
     }  
 }
--- TierheimDhiliUndJustus/DAL/Tierrasse_DA.cs
+++ TierheimDhiliUndJustus/DAL/Tierrasse_DA.cs
@@ -45,5 +45,42 @@
             }
             return tierrasselist;
         }
+
+        public static List<Tierrasse> GetTierrasseWithTierartID(int tierart_ID)
+        {
+            List<Tierrasse> tierrasselist = new List<Tierrasse>();
+
+
+            using (MySqlConnection conn = new MySqlConnection(Config.CONNSTRING))
+            {
+                conn.Open();
+
+
+                sqlstatement = "SELECT * FROM tierrasse WHERE FK_Tierart_Tierrasse = " + tierart_ID;
+
+
+                using (MySqlCommand cmd = new MySqlCommand(sqlstatement, conn))
+                {
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Tierrasse tierrasse = new Tierrasse(
+                                (int)reader["ID_Tierrasse"],
+                                (string)reader["Tierrassenamen"],
+                                (int)reader["FK_Tierart_Tierrasse"]);
+
+                            tierrasselist.Add(tierrasse);
+                        }
+
+                    }
+                }
+
+
+            }
+            return tierrasselist;
+        }
     }
 }
