65c91786be39a29bd1dcdf9b41e8f5861ce3f40e
Meli <melanie.wolf2005@gmail.com>
2022-05-10 15:07:32 +0200

    filter

---
+++ TierheimDhiliUndJustus/DAL/Tierart_DA.cs
@@ -0,0 +1,48 @@
+﻿namespace TierheimDhiliUndJustus.DAL
+{
+    using MySqlConnector;
+    using System.Drawing;
+    using TierheimDhiliUndJustus.BLL;
+    using System.Data.SqlClient;
+    using System.IO;
+
+    public static class Tierart_DA
+    {
+        public static string sqlstatement = "";
+        public static List<Tierart> GetTierarten()
+        {
+            List<Tierart> tierartlist = new List<Tierart>();
+
+
+            using (MySqlConnection conn = new MySqlConnection(Config.CONNSTRING))
+            {
+                conn.Open();
+
+
+                sqlstatement = "SELECT * FROM tierart";
+
+
+                using (MySqlCommand cmd = new MySqlCommand(sqlstatement, conn))
+                {
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Tierart tierart = new Tierart(
+                                (int)reader["ID_Tierart"],
+                                (string)reader["Tierartname"]);
+
+                            tierartlist.Add(tierart);
+                        }
+
+                    }
+                }
+
+
+            }
+            return tierartlist;
+        }
+    }
+}
---
+++ TierheimDhiliUndJustus/DAL/Tierrasse_DA.cs
@@ -0,0 +1,49 @@
+﻿namespace TierheimDhiliUndJustus.DAL
+{
+    using MySqlConnector;
+    using System.Drawing;
+    using TierheimDhiliUndJustus.BLL;
+    using System.Data.SqlClient;
+    using System.IO;
+
+    public class Tierrasse_DA
+    {
+        public static string sqlstatement = "";
+        public static List<Tierrasse> GetTierrassen(int tierart_ID)
+        {
+            List<Tierrasse> tierrasselist = new List<Tierrasse>();
+
+
+            using (MySqlConnection conn = new MySqlConnection(Config.CONNSTRING))
+            {
+                conn.Open();
+
+
+                sqlstatement = "SELECT * FROM tierrasse WHERE FK_Tierart_Tierrasse = " + tierart_ID;
+
+
+                using (MySqlCommand cmd = new MySqlCommand(sqlstatement, conn))
+                {
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Tierrasse tierrasse = new Tierrasse(
+                                (int)reader["ID_Tierrasse"],
+                                (string)reader["Tierrassenamen"],
+                                (int)reader["FK_Tierart_Tierrasse"]);
+
+                            tierrasselist.Add(tierrasse);
+                        }
+
+                    }
+                }
+
+
+            }
+            return tierrasselist;
+        }
+    }
+}
