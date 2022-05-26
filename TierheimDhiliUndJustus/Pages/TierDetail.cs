982b548c7aa5ce31377096f4b3324bfab728e5ed
Meli <melanie.wolf@hak-bregenz.at>
2022-05-26 17:57:43 +0200

    scheiß klassen alter jetzt geht garnichts mehr toll

---
+++ TierheimDhiliUndJustus/Pages/TierDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace TierheimDhiliUndJustus.Pages
+{
+    using Microsoft.AspNetCore.Components;
+    using TierheimDhiliUndJustus.BLL;
+    using TierheimDhiliUndJustus.DAL;
+    using MySqlConnector;
+    using System.Drawing;
+    using System.Data.SqlClient;
+    using System.IO;
+    using TierheimDhiliUndJustus.Pages;
+
+    public partial class Tierdetail : ComponentBase
+    {
+
+    }
+}
