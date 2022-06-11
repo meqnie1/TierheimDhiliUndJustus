b92171c8c913e8c5edb8a34f22dc8566adcc2d99
hannah <hannah.scheidbach@hak-bregenz.at>
2022-06-11 21:28:45 +0200

    Tierbild hinzufügen hilfe i könnt weinen

--- TierheimDhiliUndJustus/Pages/Tierhinzufügen.cs
+++ TierheimDhiliUndJustus/Pages/Tierhinzufügen.cs
@@ -1,4 +1,5 @@
-﻿using TierheimDhiliUndJustus.BLL;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using TierheimDhiliUndJustus.BLL;
 using TierheimDhiliUndJustus.DAL;
 
 namespace TierheimDhiliUndJustus.Pages
@@ -12,6 +13,7 @@ namespace TierheimDhiliUndJustus.Pages
         public string tierrasseText = "";
         public bool fundtier = false;
         public string beschreibung = "";
+        static string path = "img/tiere";
 
         List<Tierart> lst_tierarten = Tierart_DA.GetTierarten();
         List<Tierrasse> lst_tierrassen = Tierrasse_DA.GetTierrassen();
@@ -40,5 +42,20 @@ namespace TierheimDhiliUndJustus.Pages
             currentgeschlecht = Convert.ToString(checkedValue);
         }
 
+        protected async Task Uploadtierpicture(InputFileChangeEventArgs e)
+        {
+
+            long _maxFileSize = 20000000;
+      
+            string path = "img/tiere/";
+
+            await using (FileStream fs = new(path, FileMode.Create))
+            {
+                await e.File.OpenReadStream(_maxFileSize).CopyToAsync(fs);
+
+            }
+
+
+        }
     }
 }
